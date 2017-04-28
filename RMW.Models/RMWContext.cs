9f2b0d24f6cc589bf3e03a80eda632fcc7c94dfb
Ryan <admin@bootbaron.com>
2017-04-27 17:19:49 -0700

    add space

--- RMW.Models/RMWContext.cs
+++ RMW.Models/RMWContext.cs
@@ -5,8 +5,11 @@ namespace RMW.Models
     public class RMWContext : DbContext
     {
         public DbSet<Article> Articles { get; set; }
+
         public DbSet<Comment> Comments { get; set; }
+
         public DbSet<User> Users { get; set; }
+
         public DbSet<Role> Roles { get; set; }
 
         static RMWContext()
